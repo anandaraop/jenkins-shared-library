5114516ec694b47af1270ec95c2105e1ccf3bb12
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-27 06:36:12 +0530

    Adding permissing to ci-server for eks

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -23,6 +23,7 @@ def call() {
       stage('Release Software') {
         sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com'
         sh 'docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
+        sh 'aws eks update-kubeconfig --name dev-eks'
       }
     } else {
       stage('Lint Code') {
